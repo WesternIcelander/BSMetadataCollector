3006ccdd1b9aeaf722fcfccbd9f38aa5f76a4332
Siggi <7286178+Siggi88@users.noreply.github.com>
2025-04-18 14:08:01 +0000

    BSMDCInStream: Decompress stream if GZIP stream is detected

--- src/main/java/io/siggi/beatsaber/metadatacollector/bsmdcstream/BSMDCInStream.java
+++ src/main/java/io/siggi/beatsaber/metadatacollector/bsmdcstream/BSMDCInStream.java
@@ -1,11 +1,14 @@
 package io.siggi.beatsaber.metadatacollector.bsmdcstream;
 
+import io.siggi.tools.io.ConcatenatedInputStream;
 import io.siggi.tools.io.IO;
 
+import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.zip.GZIPInputStream;
 
 import static io.siggi.beatsaber.metadatacollector.Util.readInt;
 import static io.siggi.beatsaber.metadatacollector.Util.readLong;
@@ -20,7 +23,16 @@ public class BSMDCInStream implements Closeable {
     public BSMDCInStream(InputStream in) throws IOException {
         byte[] header = new byte[Constants.BSMDC_HEADER.length];
         IO.readFully(in, header);
+        headerCheck:
         if (!Arrays.equals(header, Constants.BSMDC_HEADER)) {
+            if (header[0] == (byte) 0x1f && header[1] == (byte) 0x8b) {
+                in = new GZIPInputStream(new ConcatenatedInputStream(Arrays.asList(new ByteArrayInputStream(header), in).iterator()));
+                header = new byte[Constants.BSMDC_HEADER.length];
+                IO.readFully(in, header);
+                if (Arrays.equals(header, Constants.BSMDC_HEADER)) {
+                    break headerCheck;
+                }
+            }
             throw new IOException("Not BSMDC file!");
         }
         fileVersion = readInt(in);
